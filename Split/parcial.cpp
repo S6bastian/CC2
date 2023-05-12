512654ead4b71a87be2d84edffb7e0ee30dbdc7d
Sebastián Sánchez <seb2001sac@gmail.com>
2023-05-11 21:29:50 -0500

    pairs no funciona

--- Split/parcial.cpp
+++ Split/parcial.cpp
@@ -5,11 +5,17 @@ using namespace std;
 void print(int *);
 void swap(int *, int*);
 void reposition(int *); //poner todos los 0 al final sin cambiar orden de los otros numeros
+void pairs(int *);//
 
 int main(){
     int A[]={1,0,3,7,89,0,345,0};
+    int B[]={4,3,3,4,3,6,78,9};
+    
     reposition(A);
     print(A);
+
+    pairs(B);
+    print(B);
 
     return 0;
 }
@@ -37,4 +43,26 @@ void reposition(int *A){
             }
         }
     }
+}
+
+void pairs(int *A){
+    int *start=A;
+    int *end=A+8;
+    int cont=1;
+    while(cont!=0){
+        cont=0;
+        for(int *i=A; i<end; i++){
+            if(*i==*i+1){
+                *i=0; *(i+1)=0;
+                for(int *j=i+1; j<end; j++){
+                    swap(j-1,j);
+                }
+                for(int *j=i+1; j<end; j++){
+                    swap(j-1,j);
+                }
+                cont++;
+            }
+        }
+    }
+    
 }
