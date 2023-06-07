25c97cdc0ea5ef6df9a4b198f1401df5ac1e67c4
Sebastián Sánchez <seb2001sac@gmail.com>
2023-06-06 21:58:49 -0500

    error de método FIND solucionado

--- Lista_enlazada/lista_enlazada_T.cpp
+++ Lista_enlazada/lista_enlazada_T.cpp
@@ -3,35 +3,38 @@
 using namespace std;
 
 template <class T>
-struct nodo{
+struct nodo {
     T valor;
-    nodo <T> *next;
-    nodo(T v, nodo<T> *n=nullptr){
-        valor=v;
-        next=n;
+    nodo <T>* next;
+    nodo(T v, nodo<T>* n = nullptr) {
+        valor = v;
+        next = n;
     }
 };
 
 
 template <class T>
-struct LE{    //Lista Enlazada
-    nodo<T> *head;
-    
+struct LE {    //Lista Enlazada
+    nodo<T>* head=NULL;
+
     bool find(T x, nodo<T> *&pos);
     void add(T x);
     bool del(T x);
     void print();
-    ~LE();
+    //~LE();
 };
 
 template <class T>
-bool LE<T>::find(T x, nodo<T> *&pos){
-    bool key=false;
-    pos=nullptr;
-    
-    for(nodo<T> *p=head; p&&p->valor<x; pos=p, p=p->next){
-        if(p->valor==x){
-            key=true;
+bool LE<T>::find(T x, nodo<T> *&pos) {
+    bool key = false;
+    pos = nullptr;
+
+    for (nodo<T>* p = head; p && p->valor <= x; p = p->next) {
+        if (p->valor == x) {
+            key = true;
+        }
+        else{
+            pos = p;
         }
     }
 
@@ -39,32 +42,32 @@ bool LE<T>::find(T x, nodo<T> *&pos){
 }
 
 template <class T>
-void LE<T>::add(T x){
-    nodo<T> *pos;
+void LE<T>::add(T x) {
+    nodo<T>* pos;
 
-    if(!find(x, pos)){
-        if(pos){
-            pos->next=new nodo<T> (x, pos->next);
+    if (!find(x, pos)) {
+        if (pos) {
+            pos->next = new nodo<T>(x, pos->next);
         }
-        else{
-            head=new nodo<T>(x,head);
+        else {
+            head = new nodo<T>(x, head);
         }
     }
 }
 
 template  <class T>
-bool LE<T>::del(T x){
-    nodo<T> *pos, *tmp;
-    bool key=find(x, pos);
+bool LE<T>::del(T x) {
+    nodo<T>* pos, * tmp;
+    bool key = find(x, pos);
 
-    if(key){
-        if(pos){
-            tmp=pos->next;
-            pos->next=tmp->next;
+    if (key) {
+        if (pos) {
+            tmp = pos->next;
+            pos->next = tmp->next;
         }
-        else{
-            tmp=head;
-            head=tmp->next;
+        else {
+            tmp = head;
+            head = tmp->next;
         }
         delete tmp;
     }
@@ -73,21 +76,22 @@ bool LE<T>::del(T x){
 }
 
 template <class T>
-void LE<T>::print(){
-    cout<<"HEAD -> ";
-    for(nodo<T>*p=head; p; p->next){
-        cout<<p->valor<<" -> ";
+void LE<T>::print() {
+    cout << "HEAD -> ";
+    for (nodo<T>* p = head; p; p=p->next) {
+        cout << p->valor << " -> ";
     }
-    cout<<"NULL"<<endl;
+    cout << "NULL" << endl;
 }
 
 
-int main(){
-    LE<int> *list;
-    list->add(5);
-    list->add(9);
-    list->del(5);
-    list->print();
+int main() {
+    LE<int> list;
+    list.add(1);
+    list.add(3);
+    list.add(5);
+    list.del(5);
+    list.print();
 
     return 0;
 }
