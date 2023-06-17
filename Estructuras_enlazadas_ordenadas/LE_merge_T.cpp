919c957eabcfb94ebfdd01b5536ef5bec2b45313
Sebastián Sánchez <seb2001sac@gmail.com>
2023-06-16 23:16:26 -0500

    Version Completa

--- Estructuras_enlazadas_ordenadas/LE_merge_T.cpp
+++ Estructuras_enlazadas_ordenadas/LE_merge_T.cpp
@@ -87,17 +87,40 @@ void LE<T>::print() {
 
 template <class T>
 void merge(nodo<T> *&H1, nodo<T> *&H2){
-    for(nodo<T> *p1 = H1, *p2 = H2, *pre1 = H1; p2;){
-        if (p2->valor < p1->valor){
-            H2 = p2->next; 
-            pre1->next = p2;
-            p2->next = p1;
+    nodo<T> *ap1, *p1, *p2;
+    ap1 = H1;
+    p1 = ap1->next;
+    p2 = H2;
+    while(H2){
+        if(p2->valor < H1->valor){
+            H2 = p2->next;
+            H1 = p2;
+            p2->next = ap1;
+            ap1 = H1;
+            p1 = ap1->next;
             p2 = H2;
         }
-        else{
-            pre1 = p1;
-            p1 = p1->next;
+        else if(p2->valor >= ap1->valor && p2->valor < p1->valor){
+            H2 = p2->next;
+            ap1->next = p2;
+            p2->next = p1;
+            ap1 = p2;
+            p2 = H2;
         }
+        
+        else{
+            ap1 = ap1->next;
+            p1 = p1->next;
+            if(p1 == nullptr){
+                break;
+            }
+        }
+        
+        
+    }
+    if(p2->valor >= ap1->valor && p1 == nullptr && p2 != nullptr){
+        ap1->next = p2;
+        H2 = nullptr;
     }
 }
 
@@ -113,11 +136,19 @@ int main() {
     H2.add(3);
     H2.add(6);
     H2.add(9);
+
+    H1.add(3);
+    H2.add(0);
+    H2.add(1);
+    H2.add(4);
+    H2.add(23);
+
     H1.print();
     H2.print();
+    cout<<endl;
 
     merge(H1.head, H2.head);
-    //H1.del(0);
+
     H1.print();
     H2.print();
 
